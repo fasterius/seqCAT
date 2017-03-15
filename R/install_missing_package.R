449a5c45fcb90684abf57e6926e07cdb9d59cb1a
Erik Fasterius <erikfas@kth.se>
2017-03-15 14:31:59 +0100

    Change functionality for loading packages
    The source file install_missing_package.R now performs the whole process
    of trying to load and installing missing packages, taking a list of all
    the required packages as input.

--- R/install_missing_package.R
+++ R/install_missing_package.R
@@ -1,32 +1,41 @@
-#' Installs missing package and loads them.
+#' Load existing packages and install missing ones.
 #'
-#' This is a function for automatic installation and loading of any R package
+#' This is a function for automatic installation and loading of R packages
 #' that are not already installed. Installation is first attempted from CRAN
 #' (\url{https://cran.r-project.org/}), followed by BioConductor
 #' (\url{http://bioconductor.org/}).
 #'
-#' @param package The package to be installed.
+#' @param packages The packages to be installed.
 #' @examples
-#' installMissingPackage('docopt')
+#' loadPackages('docopt')
+#' loadPackages(c('docopt', 'plyr'))
 
-installMissingPackage = function(package) {
+loadPackages = function(packages) {
 
-    # First try to install from CRAN ...
-    tryCatch (silent=TRUE,
-        install.package(package, repos='http://cran.us.r-project.org'),
-        
-        # ... then from BioConductor, if unsuccessful
-        warning = function(bc) {
-            source("http://bioconductor.org/biocLite.R")
-            biocLite(package)
-        },
-        error = function(bc) {
-            source("http://bioconductor.org/biocLite.R")
-            biocLite(package)
+    # Loop over every package submitted
+    for ( package in packages ) {
+
+        # Load package, install if missing
+        if ( ! require(package, character.only=TRUE) ) {
+
+            # First try to install from CRAN ...
+            tryCatch (silent=TRUE,
+                install.package(package, 
+                                repos='http://cran.us.r-project.org')
+                
+                # ... then from BioConductor, if unsuccessful
+                warning = function(bc) {
+                    source("http://bioconductor.org/biocLite.R")
+                    biocLite(package)
+                },
+                error = function(bc) {
+                    source("http://bioconductor.org/biocLite.R")
+                    biocLite(package)
+                }
+            )
+
+            # Load package after installation
+            require(package)
         }
-    )
-    
-    # Load package
-    require(package)
+    }
 }
-

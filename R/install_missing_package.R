78373ece22c1a2c448e88a3aa64acb7633671cc0
Erik Fasterius <erikfas@kth.se>
2017-03-15 14:16:26 +0100

    Change how missing packages are installed
    The installMissingPackage function now only installs a single package at
    a time, and should thus be called once for every package that is
    missing in the parent script/function.

--- R/install_missing_package.R
+++ R/install_missing_package.R
@@ -1,39 +1,32 @@
-#' Installs required packages if not already installed.
+#' Installs missing package and loads them.
 #'
-#' This is a function for automatic installation of any R packages that are
-#' not already installed. A list of required packages are submitted to
-#' \code{packages} and installed from CRAN (\url{https://cran.r-project.org/})
-#' and BioConductor (\url{http://bioconductor.org/}), if not already available
-#' to R.
+#' This is a function for automatic installation and loading of any R package
+#' that are not already installed. Installation is first attempted from CRAN
+#' (\url{https://cran.r-project.org/}), followed by BioConductor
+#' (\url{http://bioconductor.org/}).
 #'
-#' @param packages A list of packages (with package names as strings)
+#' @param package The package to be installed.
 #' @examples
-#' checkPackages('docopt')
-#' checkPackages(c('docopt','plyr'))
+#' installMissingPackage('docopt')
 
-checkPackages = function(packages) {
+installMissingPackage = function(package) {
 
-    # If any required packages are missing, install them
-    if ( length(setdiff(packages, rownames(installed.packages()))) > 0 ) {
-
-        # Output a message
-        cat('installing missing packages ...\n')
+    # First try to install from CRAN ...
+    tryCatch (silent=TRUE,
+        install.package(package, repos='http://cran.us.r-project.org'),
         
-        # First try to install from CRAN ...
-        tryCatch (silent=TRUE,
-            install.packages(setdiff(packages, rownames(installed.packages())),
-                             repos='http://cran.us.r-project.org'),
-            
-            # ... then from BioConductor, if unsuccessful
-            warning=function(bc) {
-                source("http://bioconductor.org/biocLite.R")
-                biocLite(setdiff(packages, rownames(installed.packages())))
-            },
-            error=function(bc) {
-                source("http://bioconductor.org/biocLite.R")
-                biocLite(setdiff(packages, rownames(installed.packages())))
-            }
-        )
-    }
+        # ... then from BioConductor, if unsuccessful
+        warning = function(bc) {
+            source("http://bioconductor.org/biocLite.R")
+            biocLite(package)
+        },
+        error = function(bc) {
+            source("http://bioconductor.org/biocLite.R")
+            biocLite(package)
+        }
+    )
+    
+    # Load package
+    require(package)
 }
 

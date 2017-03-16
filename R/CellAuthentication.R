7aae4544af51b78295f4b1304d02e69f3eebe6e6
Erik Fasterius <erikfas@kth.se>
2017-03-16 13:05:51 +0100

    Add CellAuthentication.R package documentation
    Add the CellAuthentication.R file for documenting the package as a
    whole, without containing any specific functions.

---
+++ R/CellAuthentication.R
@@ -0,0 +1,13 @@
+#' CellAuthentication: a package for cell authentication.
+#'
+#' The CellAuthentication package provides a number of functions for performing
+#' authentication and validation of biological samples through analysis of high
+#' throughput sequencing data.
+#' 
+#' @section Authentication functions:
+#' The Authentication functions ...
+#'
+#' @docType package
+#' @name CellAuthentication
+#' @import docopt
+NULL

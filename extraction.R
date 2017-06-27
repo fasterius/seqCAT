b16e72ec97b006d9f3c614d0d6288f0f2c2c4b31
Erik Fasterius <erikfas@kth.se>
2017-06-27 10:54:23 +0200

    Adapt extraction wrapper script to new functions
    Adapt the variant extraction wrapper script to work with the new changes
    in the extract_variants.R script; always write to file, support both
    Python and R, arguments.

--- extraction.R
+++ extraction.R
@@ -1,8 +1,5 @@
 #!/usr/bin/env Rscript
 
-# Load package
-library("CellAuthentication")
-
 # Command parser
 doc <- "
 Usage:
@@ -10,12 +7,19 @@ Usage:
 
 Options:
     -h, --help                      show this help message
+    -p, --python                    use python 
+                                        [default: false]
     -f <depth>, --filter <depth>    skip variants below <filter> depth 
                                         [default: 10]
 "
 opts <- docopt::docopt(doc)
 
+# Load package
+suppressPackageStartupMessages(library("CellAuthentication"))
+
 # Extract variants
-message(paste0("reading '", basename(opts$vcf_file), "' ..."))
-extract_variants(opts$vcf_file, opts$sample, opts$output_file, opts$filter)
-message("finished.")
+extract_variants(opts$vcf_file,
+                 opts$sample,
+                 opts$output_file,
+                 as.numeric(opts$filter),
+                 opts$python)

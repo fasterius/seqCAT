0604a515b6d138ae4bed1d999f6453f068ea122d
Erik Fasterius <erikfas@kth.se>
2017-08-03 11:21:52 +0200

    Only filter by depth in filter_variants function
    Only filter the variants by sequencing depth in the filter_variants
    function, as non-overlapping variants are removed at the variant
    overlaps stage.

--- R/filter_variants.R
+++ R/filter_variants.R
@@ -1,8 +1,8 @@
 #' Filter variants on specified criteria
 #'
-#' This is a function for filtering variants on various criteria. Variants with
-#' a depth lower than 10 are removed, as are variants that don't have a
-#' complete genotype for both samples. 
+#' This is a function for filtering variants on sequencing depth. Variants with
+#' a depth lower than 10 are removed by default, but can be changed in the
+#' function call.
 #'
 #' @param variants The data frame containing the variant data to be filtered.
 #' @param filter_depth Threshold for variant depth (default 10)
@@ -17,17 +17,6 @@ filter_variants = function(variants, filter_depth=10) {
     # Filter on sequencing depth
     variants = variants[variants$DP.input_1 >= filter_depth & 
                         variants$DP.input_2 >= filter_depth, ]
-
-    # Find variants that have complete genotypes in both samples
-    alleles = c('A1.input_1', 'A2.input_1', 
-                'A1.input_2', 'A2.input_2')
-        
-    # Find rows with data for both alleles
-    complete = row.names(
-        subset(variants,rowSums(is.na(variants[, alleles])) == 0))
-
-    # Remove non-complete genotypes
-    variants = variants[complete, ]
 
     # Return the filtered variants
     return(variants)

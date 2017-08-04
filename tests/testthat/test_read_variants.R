e2eac34d03e4587be1357e32d081022c1c83b9e8
Erik Fasterius <erikfas@kth.se>
2017-08-04 16:52:50 +0200

    Change read_variants tests to use GRanges output
    Change the read_variants tests to use the GRanges output directly
    instead of converting to dataframes.

--- tests/testthat/test_read_variants.R
+++ tests/testthat/test_read_variants.R
@@ -9,19 +9,25 @@ file_2 <- system.file("extdata",
                       "extract.sample2.txt",
                       package = "CellAuthentication")
 
-data_1 <- as.data.frame(read_variants(file        = file_1,
-                                      sample_name = "sample1"))
+granges_1 <- read_variants(file        = file_1,
+                           sample_name = "sample1")
 
-data_2 <- as.data.frame(read_variants(file        = file_2,
-                                      sample_name = "sample2"))
+granges_2 <- read_variants(file        = file_2,
+                           sample_name = "sample2")
 
 # Tests
-test_that("correct number of variants are read and de-duplicated", {
-    expect_equal(nrow(unique(data_1[c("seqnames", "start", "ENSGID")])), 383)
-    expect_equal(nrow(unique(data_2[c("seqnames", "start", "ENSGID")])), 382)
+test_that("a GRanges object is returned", {
+    expect_identical(class(granges_1)[1], "GRanges")
 })
 
-test_that("only chromosome 1-22, X and Y are read", {
-    expect_equal(nrow(data_1[!(data_1$seqnames %in% c(1:22, "X", "Y")), ]), 0)
-    expect_equal(nrow(data_2[!(data_2$seqnames %in% c(1:22, "X", "Y")), ]), 0)
+test_that("correct number of variants are read and de-duplicated", {
+    expect_equal(length(granges_1), 383)
+    expect_equal(length(granges_2), 382)
+    expect_equal(length(mcols(granges_1)), 17)
+    expect_equal(length(mcols(granges_2)), 17)
+})
+
+test_that("non-standard chromosomes are removed correcty", {
+    expect_identical(levels(seqnames(granges_1)), "1")
+    expect_identical(levels(seqnames(granges_2)), "1")
 })

17ad55cf12df9d3390fc071beaceec2e18aba8d8
Erik Fasterius <erikfas@kth.se>
2017-08-28 08:58:33 +0200

    Add unit tests for read_cosmic function

---
+++ tests/testthat/test_9_read_cosmic.R
@@ -0,0 +1,29 @@
+library("CellAuthentication")
+context("Read COSMIC data")
+
+# Input file
+file <- system.file("extdata",
+                    "subset_CosmicCLP_MutantExport.tsv.gz",
+                    package = "CellAuthentication")
+
+# Read COSMIC data
+cosmic <- read_cosmic(file, "HCT116")
+cosmic_list <- list_cosmic(file)
+
+# Tests
+test_that("the returned object is a GRanges object", {
+    expect_identical(class(cosmic)[1], "GRanges")
+})
+
+test_that("the returned number of variants and metadata columns are correct", {
+    expect_equal(length(cosmic), 1)
+    expect_equal(length(mcols(cosmic[cosmic$sample == "HCT116", ])), 12)
+})
+
+test_that("a character list of cell lines are correctly returned", {
+    expect_identical(class(cosmic_list), "character")
+})
+
+test_that("correct number of cell lines are listed", {
+    expect_equal(length(cosmic_list), 164)
+})

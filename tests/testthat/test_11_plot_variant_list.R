80a1768204023495eebcddc52b6f9cb6080c08fa
Erik Fasterius <erikfas@kth.se>
2017-11-30 14:52:56 +0100

    Add unit tests for `plot_variant_list`
    Add unit tests for the `plot_variant_list` function, and add the
    associated test data and documentation.

---
+++ tests/testthat/test_11_plot_variant_list.R
@@ -0,0 +1,23 @@
+library("seqCAT")
+context("Plot lists of known variants")
+
+# Load test data
+data(test_variant_list)
+
+# Plots
+plot <- plot_variant_list(test_variant_list)
+plot_obj <- ggplot2::ggplot_build(plot)
+
+# Tests
+test_that("correct variant/gene combinations are being plotted", {
+    expect_equal(plot_obj$layout$panel_ranges[[1]]$x.labels[1], "1:16229 (DDX11L1)")
+    expect_equal(plot_obj$layout$panel_ranges[[1]]$x.labels[2], "1:16298 (DDX11L1)")
+    expect_equal(plot_obj$layout$panel_ranges[[1]]$y.labels[1], "sample1")
+    expect_equal(plot_obj$layout$panel_ranges[[1]]$y.labels[2], "sample2")
+})
+
+test_that("genotypes are separated and coloured correctly", {
+    expect_equal(plot_obj$data[[1]][1, "fill"], "#999999")
+    expect_equal(plot_obj$data[[2]][3, "fill"], "#4e8ce4")
+    expect_equal(plot_obj$data[[3]][12, "fill"], "#a6c6f2")
+})

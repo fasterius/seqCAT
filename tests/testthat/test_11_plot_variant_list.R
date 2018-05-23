ae27208850f8b2e883020c041fa3823d211d382e
Erik Fasterius <erikfas@kth.se>
2018-05-23 16:48:52 +0200

    Fix issues with the upcoming 2.3.0 ggplot2 version
    Fix several issues that breaks `seqCAT` under the upcoming `ggplot2`
    2.3.0 version (2.2.1.9000 devel). Most were to do with the change of
    `panel_ranges` to `panel_params` from the `ggplot_build()`, which
    affected unit tests, but one were due to increased stringency in input
    data for plotting objects: the `plot_impacts()` function previously
    added new rows to a grouped dataframe. This is now fixed by ungrouping
    the dataframe first.

--- tests/testthat/test_11_plot_variant_list.R
+++ tests/testthat/test_11_plot_variant_list.R
@@ -4,16 +4,23 @@ context("Plot lists of known variants")
 # Load test data
 data(test_variant_list)
 
-# Plots
+# Create plot object
 plot <- plot_variant_list(test_variant_list)
 plot_obj <- ggplot2::ggplot_build(plot)
 
+# Get plot parameters based on ggplot2 version
+if (utils::packageVersion("ggplot2") < "2.2.1.9000") {
+    plot_params <- plot_obj$layout$panel_ranges[[1]]
+} else {
+    plot_params <- plot_obj$layout$panel_params[[1]]
+}
+
 # Tests
 test_that("correct variant/gene combinations are being plotted", {
-    expect_equal(plot_obj$layout$panel_ranges[[1]]$x.labels[1], "1:16229 (DDX11L1)")
-    expect_equal(plot_obj$layout$panel_ranges[[1]]$x.labels[2], "1:16298 (DDX11L1)")
-    expect_equal(plot_obj$layout$panel_ranges[[1]]$y.labels[1], "sample1")
-    expect_equal(plot_obj$layout$panel_ranges[[1]]$y.labels[2], "sample2")
+    expect_equal(plot_params$x.labels[1], "1:16229 (DDX11L1)")
+    expect_equal(plot_params$x.labels[2], "1:16298 (DDX11L1)")
+    expect_equal(plot_params$y.labels[1], "sample1")
+    expect_equal(plot_params$y.labels[2], "sample2")
 })
 
 test_that("genotypes are separated and coloured correctly", {

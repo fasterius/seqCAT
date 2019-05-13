94a9592fac44914424181bce32fce28cc9ce43f2
Erik Fasterius <erik.fasterius@outlook.com>
2019-05-13 11:56:14 +0200

    Remove redundant unit testing code

--- tests/testthat/test_02_read_profile.R
+++ tests/testthat/test_02_read_profile.R
@@ -5,15 +5,12 @@ context("Read SNV profiles")
 file_1 <- system.file("extdata", "test_1.profile.txt.gz", package = "seqCAT")
 file_2 <- system.file("extdata", "test_2.profile.txt.gz", package = "seqCAT")
 file_3 <- system.file("extdata", "test_3.profile.txt.gz", package = "seqCAT")
-file_4 <- system.file("extdata", "test_4.profile.txt.gz", package = "seqCAT")
 profile_dir <- system.file("extdata", package = "seqCAT")
 
 # Read profiles
 test_1 <- suppressMessages(read_profile(file = file_1))
 test_2 <- suppressMessages(read_profile(file = file_2))
 test_3 <- suppressMessages(read_profile(file = file_3))
-test_4 <- suppressMessages(read_profile(file = file_4))
-test_5 <- suppressMessages(read_profile(file = file_4))
 test_dir <- suppressMessages(read_profiles(profile_dir = profile_dir))
 
 # Tests
@@ -31,8 +28,3 @@ test_that("correct number of variants are read", {
 test_that("profiles without variant annotations are handled correctly", {
     expect_equal(nrow(test_3), 99)
 })
-#
-# test_that("empty profiles are handled correcty", {
-    # expect_equal(nrow(test_4), 1)
-    # expect_identical(test_4$sample, "sample4")
-# })

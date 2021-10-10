705dfcad793bb17c439390d09570322a1597e6ec
Erik Fasterius <erik.fasterius@outlook.com>
2021-10-10 17:51:56 +0200

    Fix bug in new versions of tidyr
    Fix a big related to how `unnest()` works in newer versions of tidyr:
    usage of `unnest_longer` is now needed.

--- R/create_profile.R
+++ R/create_profile.R
@@ -181,7 +181,7 @@ create_profile <- function(vcf_file,
 filter_annotations <- function(data) {
 
     # Separate ANN into rows
-    data <- tidyr::unnest(data, !!rlang::sym("ANN"))
+    data <- tidyr::unnest_longer(data, !!rlang::sym("ANN"))
 
     # Separate ANN into columns
     data <- tidyr::separate(data,
